c964f1823a4412eea2c0af2644fae10aec86c354
juliapd23 <47598354S@iespoblenou.org>
2024-12-04 12:20:35 +0100

    "Add divide method without the error"

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,12 +3,15 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
     public int subtract(int a, int b) {
         return a - b;
     }
+
     public int multiply(int a, int b) {
         return a * b;
     }
+
     public int divide(int a, int b) {
         if ((a == 0) || (b == 0)) {
             System.out.println("Error");
@@ -17,6 +20,7 @@ public class Calculator {
             return a / b;
         }
     }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(10, 5));
@@ -24,3 +28,4 @@ public class Calculator {
         System.out.println("Multiplication: " + calculator.multiply(10, 5));
         System.out.println("Division: " + calculator.divide(10, 5));
     }
+}
